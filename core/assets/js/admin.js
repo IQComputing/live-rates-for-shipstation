dc16340a4b9ec2fced1cde9e09fa101c3deade5f
Howdy_McGee <Axel.Miller98@gmail.com>
2025-11-04 00:03:03 -0600

    Removes some modal testing scripts.

--- core/assets/js/admin.js
+++ core/assets/js/admin.js
@@ -16,17 +16,17 @@ if( document.getElementById( 'woocommerce_shipstation_iqlrss_api_key' ) ) {
 if( document.getElementById( 'woocommerce_iqlrss_shipstation_title' ) ) {
 
 	// @todo Remove
-	document.querySelector( '[name="woocommerce_iqlrss_shipstation_packing"]' ).value = 'wc-box-packer';
-	document.querySelector( '[name="woocommerce_iqlrss_shipstation_packing"]' ).dispatchEvent( new Event( 'change' ) );
+	// document.querySelector( '[name="woocommerce_iqlrss_shipstation_packing"]' ).value = 'wc-box-packer';
+	// document.querySelector( '[name="woocommerce_iqlrss_shipstation_packing"]' ).dispatchEvent( new Event( 'change' ) );
 
 	import( './shipping-zones/_main.js' ).then( ( Module ) => {
 
 		new Module.shippingZoneSettings();
 
 		// @todo Remove
-		setTimeout( () => {
-			document.querySelector( '[data-iqlrss-modal]' ).dispatchEvent( new Event( 'click', { bubbles: true } ) );
-		}, 500 );
+		// setTimeout( () => {
+		// 	document.querySelector( '[data-iqlrss-modal]' ).dispatchEvent( new Event( 'click', { bubbles: true } ) );
+		// }, 500 );
 
 	} );
 }
