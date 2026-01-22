2d4ba10a2c03730771cb82970f799f822de483a4
Howdy_McGee <Axel.Miller98@gmail.com>
2026-01-21 23:12:26 -0600

    Autoloader change.

--- live-rates-for-shipstation.php
+++ live-rates-for-shipstation.php
@@ -236,29 +236,8 @@ class Driver {
 
 
 /**
- * Class Autoloader
- *
- * @param String $class
+ * Autoload and Drive!
  */
-spl_autoload_register( function( $class ) {
-
-	if( false === strpos( $class, __NAMESPACE__ . '\\' ) ) {
-		return $class;
-	}
-
-	$class_path	= str_replace( __NAMESPACE__ . '\\', '', $class );
-	$class_path	= str_replace( '_', '-', strtolower( $class_path ) );
-	$class_path	= str_replace( '\\', '/', $class_path );
-	$file_path	= wp_normalize_path( sprintf( '%s/%s',
-		rtrim( plugin_dir_path( __FILE__ ), '\\/' ),
-		$class_path . '.php'
-	) );
-
-	if( file_exists( $file_path ) ) {
-		require_once $file_path;
-	}
-
-} );
 require_once rtrim( __DIR__, '\\/' ) . '/_autoload.php';
 add_action( 'plugins_loaded', array( '\IQLRSS\Driver', 'drive' ), 8 );
 
