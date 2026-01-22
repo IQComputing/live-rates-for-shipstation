9a5e18aebb97ceebcffbab86b56c2302bb4cea58
Howdy_McGee <Axel.Miller98@gmail.com>
2026-01-21 22:22:52 -0600

    More WordPress Mockeries

--- tests/Mockeries/WordPress.php
+++ tests/Mockeries/WordPress.php
@@ -13,6 +13,7 @@ function wp_json_encode( $data ) { return json_encode( $data ); }
 function get_option( $key, $default = '' ) { return get_data( 'Options' )[ $key ] ?? $default; }
 function wp_normalize_path( $path ) { return str_replace( array( '/', '\\', '\\\\', ), '/', $path ); }
 function plugin_dir_path( $file ) { return rtrim( dirname( $file ), '\\/' ) . '/'; }
+function maybe_unserialize( $data ) { return ( is_string( $data ) ) ? @unserialize( trim( $data ) ) : $data; }
 
 
 
@@ -23,6 +24,10 @@ function wp_cache_get( $key, $group = '', $force = false, $found = null ) { retu
 function wp_cache_set( $key, $data, $group = '', $expires = 0 ) { return true; }
 function apply_filters( $filter, $return, ...$args ) { return $return; }
 function do_action( $action, ...$args ) { return null; }
+function get_transient( $string ) { return array(); }
+function set_transient( $transient, $value, $expiration ) { return true; }
+function register_deactivation_hook( $file, $callback ) { /* Do thing */ };
+function register_activation_hook( $file, $callback ) { /* Do thing */ };
 
 function esc_html__( $string, $textdomain = '' ) {
     if( empty( $domain ) ) throw new \Exception( 'esc_html__() missing a textdomain.', 500 );
